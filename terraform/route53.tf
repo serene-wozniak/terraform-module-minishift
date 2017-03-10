3d65ec2f8c034ece8f654ebb0fe94f3971f6a1d1
Miles Wilson <miles.wilson@opencredo.com>
2017-03-10 17:00:51 +0000

    properly check name

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "vault" {
   provider = "aws.dns"
   zone_id  = "${var.route53_zone_id}"
-  name     = "minishift${var.name ? "-${var.name}" : ""}.${var.team}.${var.route53_domain}"
+  name     = "minishift${var.name == "" ? "-${var.name}" : ""}.${var.team}.${var.route53_domain}"
   type     = "A"
   ttl      = "300"
   records  = ["${aws_instance.minishift.private_ip}"]
