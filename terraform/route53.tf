9254a303be5fdf4e859226e9def70f111608a429
Miles Wilson <miles.wilson@opencredo.com>
2017-04-04 13:16:47 +0100

    add wildcard url

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -6,3 +6,12 @@ resource "aws_route53_record" "vault" {
   ttl      = "300"
   records  = ["${aws_instance.minishift.private_ip}"]
 }
+
+resource "aws_route53_record" "wildcard" {
+  provider = "aws.dns"
+  zone_id  = "${var.route53_zone_id}"
+  name     = "*.minishift${var.name == "" ? "" :  "-${var.name}"}.${var.team}.${var.route53_domain}"
+  type     = "A"
+  ttl      = "300"
+  records  = ["${aws_instance.minishift.private_ip}"]
+}
